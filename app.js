64ea64509dcf81eb40caed9580870dba3fce98c9
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-10 07:05:45 +0800

    Setup database

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connection');
 
 app.use(express.json());
 
@@ -11,4 +12,13 @@ app.get('/hello', (req, res) => {
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
