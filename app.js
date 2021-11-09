c2e602030e8e01b18caa92a1717e6020f57876f4
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-09 19:00:32 +0800

    Impl: Add sample routes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
 app.get('/hello', (req, res) => {
   res.send('Test');
 });
+
+app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
